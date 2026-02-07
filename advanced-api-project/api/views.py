f33d731aa23d84e8c75c4f41381b78abbb11d74f
esther apalara <apalaraesther@gmail.com>
2026-02-07 22:07:21 +0000

    Task 3 completed

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # List all books - anyone can view
