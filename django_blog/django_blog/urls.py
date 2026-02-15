f89524dd04f58ed568e4ecadf5bb742ebea0359e
esther apalara <apalaraesther@gmail.com>
2026-02-15 14:49:00 +0000

    task 1 completed

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name='post-delete'),
     path('profile/', profile, name='profile'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
