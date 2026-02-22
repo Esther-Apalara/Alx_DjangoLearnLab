5159a440dbb15fa57ddcd6010ffa957ec36fb295
esther apalara <apalaraesther@gmail.com>
2026-02-22 20:23:25 +0000

    task 0 completed

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="register"),
-    path("login", LoginView.as_view(), name="login"),
-    path("profile", ProfileView.as_view(), name="profile"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("profile/", ProfileView.as_view(), name="profile"),
     path("follow/<int:user_id>/", follow_user, name="follow-user"),
     path("unfollow/<int:user_id>/", unfollow_user, name="unfollow-user"),
 ]
