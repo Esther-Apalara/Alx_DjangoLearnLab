fadb0281d64810a538652972e64dea3b2b33b15c
esther apalara <apalaraesther@gmail.com>
2026-02-22 16:34:07 +0000

    task 0 completed

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,38 +1,37 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
-
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = (
-            'id',
-            'username',
-            'email',
-            'password',
-            'bio',
-            'profile_picture',
-        )
+        fields = ("username", "email", "password", "bio", "profile_picture")
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
+        user = get_user_model().objects.create_user(
+            username=validated_data.get("username"),
+            email=validated_data.get("email"),
+            password=validated_data.get("password"),
+        )
+        user.bio = validated_data.get("bio", "")
+        user.profile_picture = validated_data.get("profile_picture", None)
         user.save()
+
+        Token.objects.create(user=user)
         return user
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            'id',
-            'username',
-            'email',
-            'bio',
-            'profile_picture',
-        )
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data.get("username"), password=data.get("password"))
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        data["user"] = user
+        return data
